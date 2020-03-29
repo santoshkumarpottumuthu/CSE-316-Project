ca2e0fc9a8d15fd2749e3f73a87f0cd437cdedb2
Santosh Kumar Pottumuthu <42711522+santoshkumarpottumuthu@users.noreply.github.com>
2020-03-29 21:13:21 +0530

    os project.cpp

--- os.c
+++ os.c
@@ -1,158 +1,325 @@
-#include<stdio.h>
-#include<conio.h>
+#include<iostream>
+#include <bits/stdc++.h>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+ class process{ ///displaying the p_data pattern.
+	public:
+	int num;		// Total number of Processes 
+	int p_id; 		// enter process id's
+	int arr_time; 	// arrival time of the process
+	int burst_time; // burst_time 
+	int priority; //priority
+	int fin_time; //finish time of the project
+	int rem_time; //time remaining for the process
+	int wait_time; //waiting time
+	int start_time; //start time of the process
+	int respo_time; // starttym - arrival time.
+	int t_a_time; //turn around time
+	int avg_tat;
+	int avg_wait_time;
+	int total_t_a_time;
+	int total_wait_time;
+};  
+struct processes{
+	public:
+	int num;		// Total number of Processes 
+	int p_id; 		// enter process id's
+	int arr_time; 	// arrival time of the process
+	int burst_time; // burst_time 
+	int priority; //priority
+	int fin_time; //finish time of the project
+	int rem_time; //time remaining for the process
+	int wait_time; //waiting time
+	int start_time; //start time of the process
+	int respo_time; // starttym - arrival time.
+	int t_a_time; //turn around time
+	int avg_tat;
+	int avg_wait_time;
+	int total_t_a_time;
+	int total_wait_time;
+};
+struct processes curr;
+typedef struct processes pd;
+bool pidsort(const pd &a , const pd &b)
+{
+	return a.p_id < b.p_id;
+}
+const arrsort(const pd&a ,const pd&b)
+{
+	if(a.arr_time < b.arr_time)
+		return a.arr_time;
+	else if(a.arr_time > b.arr_time)
+		return b.arr_time;
+	if(a.priority < b.priority)
+		return a.priority;
+	else if(a.priority > b.priority)
+		return b.priority;
+	if(a.p_id < b.p_id)
+		return a.p_id;
+
+	return b.p_id;
+}
+void arrsortcheck(vector <pd> asort)
+{cout<<"p_id:"<<"\t arr_time \t"<<"burst_time \t"<<"priority"<<endl;
+	for(unsigned int m=0 ;m<asort.size();m++ )
+	{
+		
+		cout<<asort[m].p_id<<"\t"<<asort[m].arr_time<<"\t\t"<<asort[m].burst_time<<"\t\t"<<asort[m].priority<<endl;;
+	}
+}
+struct compare
+{
+	bool operator()(const pd& a ,const pd&b)
+	{
+		if( a.priority > b.priority )
+			return true;
+		else if( a.priority < b.priority )
+			return false;
+		if(a.p_id > b.p_id )
+			return true;
+
+		return false;
+		
+	}
+	
+};
+bool num__sort( const pd&a ,const pd& b)
+{
+	return a.num < b.num;
+}
+bool p_idsort(const pd&a , const pd&b)
+{
+	return a.p_id < b.p_id;
+}
 int main()
 {
-  printf("\t\t\t----------------------- Scheduling -----------------------\n\n\n\n");
-  long int n,i=0,j=0;
-  printf("Enter Number of Processes : ");
-  scanf("%ld",&n ); 
-  double priority[n],avg_waiting,avg_turnaround,burstTime[n],arrivalTime[n],waitingTime[n],turnaroundTime[n], process[n], temp, completionTime[n],min,sum=0,sum2=0,wait_final, turnaround_final, wait_avg, turnaround_avg;
-  for(i=0;i<n;i++)
-  {
-    printf("\nEnter Burst Time for Process [%d] : ", i+1 );
-    scanf("%lf", &burstTime[i]);
-    printf("Enter Arrival Time for Process [%d] : ", i+1 );
-    scanf("%lf", &arrivalTime[i] );
-    process[i]=i+1;
-  }
-
-  printf("\n\n\t\t\t -------------- Entered Values are --------------\n\n");
-  printf("\t\t\t---------------------------------------\n");
-  printf("\t\t\t| Process | Arrival Time | Burst Time |\n");
-  printf("\t\t\t---------------------------------------\n");
-  for(i=0;i<n;i++)
-  {
-    printf("\t\t\t|  P[%0.0lf]   |       %0.0lf      |     %0.0lf      |\n",process[i],arrivalTime[i],burstTime[i]);
-  }
-    printf("\t\t\t---------------------------------------\n");
-
-
-  printf("\n\n\t\t\t-------- Sorting Processes according to Arrivaltime --------\n");
-
-  for(i=0;i<n;i++)
-  {
-    for(j=0;j<n;j++)
-    {
-      if(arrivalTime[i]<arrivalTime[j])
-      {
-        
-        temp = burstTime[j];
-        burstTime[j] = burstTime[i];
-        burstTime [i] = temp;
+	int m,n,time;
+	vector <pd> p_data;
+	pd temp;
+	int fpq_process =0;
+	int rrq_process =0;
+	int p_id;
+	int arr_time;
+	int burst_time;
+	int priority;
+	int total_exec_time =0;
+	int total_t_a_time;
+	int total_wait_time;
+	cout<<"---------------------------------------------------------------------------------------"<<endl;
+	cout<<"---------------------------------------------------------------------------------------"<<endl;
+	cout<<"\t\t\t\tEnter the Total Number of Processes. \n";
 	
-	      temp = process[j];
-        process[j] = process[i];
-        process[i] = temp;
-
-	      temp = arrivalTime[j];
-        arrivalTime[j] = arrivalTime[i];
-        arrivalTime[i] = temp;
-      
-      }
-    }
-  }
-  printf("\n\n\t\t\t -------------- Now Values are --------------\n\n");
-  printf("\t\t\t---------------------------------------\n");
-  printf("\t\t\t| Process | Arrival Time | Burst Time |\n");
-  printf("\t\t\t---------------------------------------\n");
-  for(i=0;i<n;i++)
-  {
-    printf("\t\t\t|  P[%0.0lf]   |       %0.0lf      |     %0.0lf      |\n",process[i],arrivalTime[i],burstTime[i]);
-  }
-    printf("\t\t\t---------------------------------------\n");
-
-
-/*Arranging the table according to Burst time,
-Execution time and Arrival Time
-Arrival time <= Execution time
-*/
-
-
-  long int k = 1;
-  double b_time = 0;
-  for(j=0;j<n;j++)
-  {
-    b_time = b_time + burstTime[j];
-    min = burstTime[k];
-
-    for(i=k;i<n;i++)
-    {
-      if((b_time >= arrivalTime[i])&&(burstTime[i]<min))
-      {
-        temp = burstTime[k];
-        burstTime[k] = burstTime[i];
-        burstTime[i] = temp;
-
-        temp = arrivalTime[k];
-        arrivalTime[k] = arrivalTime[i];
-        arrivalTime[i] = temp;
-
-        temp = process[k];
-        process[k] = process[i];
-        process[i] = temp;
-      }
-    }
-    k++;
-  }
-  waitingTime[0] = 0;
-  for(i=1;i<n;i++)
-  {
-    sum += burstTime[i-1];
-    waitingTime[i] = sum - arrivalTime[i];
-    wait_final += waitingTime[i]; 
-  }
-  wait_avg = wait_final/n;
-  for(i=0;i<n;i++)
-  {
-    sum2 += burstTime[i];
-    turnaroundTime[i] = sum2 - arrivalTime[i];
-    turnaround_final += turnaroundTime[i];
-  }
-  turnaround_avg=turnaround_final/n;
-printf("\n\n\t\t\t -------------- Now Values are --------------\n\n");
-  printf("\t\t\t-----------------------------------------------------------------------------\n");
-  printf("\t\t\t| Process | Arrival Time | Burst Time |  Waiting Time  |  Turn Around Time  |\n");
-  printf("\t\t\t-----------------------------------------------------------------------------\n");
-  for(i=0;i<n;i++)
-  {
-    printf("\t\t\t|  P[%0.0lf]   |       %0.0lf      |     %0.0lf      |        %0.0lf       |         %0.0lf          |\n",process[i],arrivalTime[i],burstTime[i],waitingTime[i],turnaroundTime[i]);
-  }
-    printf("\t\t\t-----------------------------------------------------------------------------\n");
-
-
-
-  /*Now we have to prioritize the processes according to the formulae
-      Priority = 1+ Waiting time / Estimated run time
-  */
- 
-  
-  completionTime[0] = burstTime[0];
-  for(i=1;i<n;i++)
-  {
-    completionTime[i] = completionTime[i-1] + burstTime[i];
-  }
-
-  for(i=0;i<n;i++)
-  {
-    priority[i] = 1+waitingTime[i]/completionTime[i];
-    printf("%lf\n",priority[i]);
-  }
-
-
-  printf("\n\n\t\t\t -------------- Final Values are --------------\n\n");
-  printf("\t\t\t-----------------------------------------------------------------------------\n");
-  printf("\t\t\t| Process | Arrival Time | Burst Time |  Waiting Time  |  Turn Around Time  |\n");
-  printf("\t\t\t-----------------------------------------------------------------------------\n");
-  printf("\t\t\t|  P[%0.0lf]   |       %0.0lf      |     %0.0lf      |        %0.0lf       |         %0.0lf          |\n",process[0],arrivalTime[0],burstTime[0],waitingTime[0],turnaroundTime[0]);
-  for(i=n-1;i>0;i--)
-  {
-    printf("\t\t\t|  P[%0.0lf]   |       %0.0lf      |     %0.0lf      |        %0.0lf       |         %0.0lf          |\n",process[i],arrivalTime[i],burstTime[i],waitingTime[i],turnaroundTime[i]);
-  }
-    printf("\t\t\t-----------------------------------------------------------------------------\n");
-
-  printf("\n\n\n\t\t\tAverage Turn Around Time : %lf",turnaround_avg);
-  printf("\n\t\t\tAverage Waiting Time     : %lf\n\n",wait_avg);
+	cin>>n;
+	cout<<"The Sequence of Inputs is: \n";
+	cout<<" 1. Enter the Process id: \t 2.Arrival time \t 3.Burst_time \t 4.Priority   \n";
+	cout<<"Enter all the details of one Process then only go with the next process \n";
+	process p[n];
 	
-  getch();
-  return 0;
+	for(m=0;m<n;m++)
+	{
+		cin>>p_id>>arr_time>>burst_time>>priority;
+		temp.num = m+1;
+		temp.arr_time = arr_time;
+		temp.burst_time = burst_time;
+		temp.rem_time = burst_time;
+		temp.p_id = p_id;
+		temp.priority = priority;
+		p_data.push_back(temp);
+		p[m].p_id = p_id;
+		p[m].arr_time = arr_time;
+		p[m].burst_time = burst_time;
+		p[m].priority =priority; 
+		p[m].rem_time = burst_time;
+	}
+	sort(p_data.begin(),p_data.end(),arrsort);
+	cout<<"Enter Time Quantum Value : should be Multiple of 2 . \n";
+	int timequant;
+	cin>>timequant;
+	cout<<"Given Inputs:"<<endl;
+	cout<<"p_id"<<"\t"<<"arr_time"<<"\t"<<"burst_time"<<"\t"<<"priority"<<endl;
+	for(m=0;m<n;m++)
+	{
+			cout<<p[m].p_id<<"\t"<<p[m].arr_time<<"\t\t"<<p[m].burst_time<<"\t\t"<<p[m].priority<<endl;   //here p_data is assigned to corresponding obj.
+	}
+	
+	cout<<"After Arrival Sort:"<<endl;
+	arrsortcheck(p_data);
+	total_exec_time = total_exec_time + p_data[0].arr_time;
+	 for( m= 0 ;m< n; m++ )
+    {
+    	if( total_exec_time >= p_data[m].arr_time )
+    	{
+    		total_exec_time = total_exec_time +p_data[m].burst_time;
+    	}
+    	else
+    	{
+    		int dif = (p_data[m].arr_time - total_exec_time);
+    		total_exec_time = total_exec_time + dif + burst_time;
+
+    	}
+    }
+    int ghantt[total_exec_time] = {0};
+    for( m= 0; m< total_exec_time; m++ )
+	{
+		ghantt[m]=-1;
+	}
+	priority_queue < pd ,vector<processes> ,compare> fpq;
+	queue< pd > rrq;
+	int cpu__state =0;
+	curr.p_id = -3;
+	curr.priority = 99999;
+	int time_quant =timequant;
+	for ( time = 0; time< total_exec_time; time++ )
+	{
+		/**Insert the process with same Arrival time in Priority Queue**/
+		for( int j = 0; j< n ; j++ )
+		{
+			if(time == p_data[j].arr_time)
+			{
+				fpq.push(p_data[j]);
+			}
+		}
+		
+
+		if(cpu__state == 0) //If CPU idle
+		{
+			if(!fpq.empty())
+			{
+				curr = fpq.top();
+				cpu__state = 1;
+				fpq_process = 1;
+				fpq.pop();
+				time_quant = timequant; 
+			}
+			else if(!rrq.empty())
+			{
+				curr = rrq.front();
+				cpu__state = 1;
+				rrq_process = 1;
+				rrq.pop();
+				time_quant = timequant;
+			}
+		}
+		else if(cpu__state == 1) //If cpu has any procss
+		{
+			if(fpq_process == 1 && (!fpq.empty()))
+			{
+				if(fpq.top().priority < curr.priority ) //If new process has high priority
+				{
+					rrq.push(curr); //push curr in RQ
+					curr = fpq.top();
+					fpq.pop();
+					time_quant = timequant; 
+				}
+			}
+			else if(rrq_process == 1 && (!fpq.empty())) //If process is from RQ and new process come  in PQ
+			{
+				rrq.push(curr);
+				curr = fpq.top();
+				fpq.pop();
+				rrq_process = 0;
+				fpq_process = 1;
+				time_quant = timequant ;
+			}
+			
+
+		}
+
+
+		if(curr.p_id != -3) // Process Execution
+		{
+			curr.rem_time--;
+			time_quant--;
+			ghantt[time] = curr.p_id;
+			if(curr.rem_time == 0) //If process Finish
+			{
+				cpu__state = 0 ;
+				time_quant = 4 ;
+				curr.p_id = -3;
+				curr.priority = 99999;
+				rrq_process = 0;
+				fpq_process = 0;
+			}
+			else if(time_quant == 0 ) //If time Qunatum of a curr running process Finish
+			{
+				rrq.push(curr);
+				curr.p_id = -3;
+				curr.priority = 99999;
+				rrq_process = 0;
+				fpq_process = 0;
+				cpu__state=0;
+
+			}
+
+		}
+		
+	}
+	sort( p_data.begin(), p_data.end(), p_idsort );//sort according to p_id
+	for(int m=0;m<n;m++)//assigning finish time
+	{
+		for(int t=total_exec_time;t>=0;t--)
+		{
+			if(ghantt[t]==m+1)
+			{
+				p_data[m].fin_time=t+1;
+				break;
+
+			}
+		}
+	}
+	for(int m=0;m<n;m++) //now assigning start time to each process 
+	{
+		for(int t=0;t<total_exec_time;t++)
+		{
+
+			if(ghantt[t]==m+1)
+			{
+				p_data[m].start_time=t;
+				break;
+			}
+		}
+	}
+	
+	sort( p_data.begin(), p_data.end(), num__sort ); //again sorting on the basis of number which is defined at the start
+	for(int m=0;m<n;m++)
+	{	
+		p_data[m].respo_time=p_data[m].start_time-p_data[m].arr_time;
+		//p_data[m].wait_time=(p_data[m].fin_time-p_data[m].arr_time)-p_data[m].burst_time;
+		p_data[m].t_a_time = p_data[m].fin_time - p_data[m].arr_time;
+		p_data[m].wait_time = p_data[m].t_a_time - p_data[m].burst_time;
+		total_t_a_time += p_data[m].t_a_time;
+		total_wait_time += p_data[m].wait_time;
+		
+	}
+	cout<<"Note : \n Turn around time = finish_time - arrival_time"<<endl;
+   	cout<<" wait time = Turn around time - finish_time"<<endl;
+	float avg_t_a_time = total_t_a_time/n;
+	float avg_wait_time = total_wait_time /n;
+	cout<<"------------------------------------------------------------------------------------"<<endl;
+	cout<<"-------------------------------Final Output-----------------------------------------"<<endl;
+	cout<<"------------------------------------------------------------------------------------"<<endl;
+	
+	cout<<"p_id\t"<<"arr_time\t"<<"burst_time\t"<<"priority\t"<<"fin_time\t"<<"respo_time\t"<<"turnaroundt\t"<<"wait_time"<<endl;
+	
+	for(int m=0;m<n;m++)
+	{
+		cout<<"P"<<p_data[m].p_id<<"\t"<<p_data[m].arr_time<<"\t\t"<<p_data[m].burst_time<<"\t\t"<<p_data[m].priority<<"\t\t"<<p_data[m].fin_time<<"\t\t"<<p_data[m].respo_time<<"\t\t"<<p_data[m].t_a_time<<"\t\t"<<p_data[m].wait_time<<endl;
+		
 }
+//calculating the avg_t_a_time and avg_wait_time
+		cout<<"---------------------------------------------------------------------------------"<<endl;
+		cout<<"----------------------------------------------------------------------------------"<<endl;
+		cout<<"avg_t_a_time = "<<setprecision(3)<<avg_t_a_time<<endl;
+		cout<<"avg_wait_time ="<<setprecision(3)<<avg_wait_time<<endl;
+return 0;
+}
+
+
+
+
+
+
+
+
